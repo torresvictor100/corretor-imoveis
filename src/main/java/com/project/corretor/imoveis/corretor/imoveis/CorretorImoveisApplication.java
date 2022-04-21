27a205a20ccb5f226bb72a03dcd4f6382bea55d5
João Victor Torres <torresvictor100@gmail.com>
2022-04-21 19:59:09 -0300

    iniciando projeto

---
+++ src/main/java/com/project/corretor/imoveis/corretor/imoveis/CorretorImoveisApplication.java
@@ -0,0 +1,13 @@
+package com.project.corretor.imoveis.corretor.imoveis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorretorImoveisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorretorImoveisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/corretor/imoveis/corretor/imoveis/CorretorImoveisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.corretor.imoveis.corretor.imoveis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorretorImoveisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
