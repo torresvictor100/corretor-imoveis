bb43ad151393c3816faa3c64570f37d78a6c9381
João Victor Torres <torresvictor100@gmail.com>
2022-04-24 16:38:10 -0300

    acrescentando mais algumas entity

---
+++ src/main/java/com/project/corretor/imoveis/corretor/imoveis/entity/User.java
@@ -0,0 +1,61 @@
+package com.project.corretor.imoveis.corretor.imoveis.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@Entity
+@Table(name = "user")
+public class User {
+
+	@JsonProperty(access = JsonProperty.Access.READ_ONLY)
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "userName", unique = true, nullable = true)
+	private String userName;
+
+
+
+	@JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+	@Column(name = "password", nullable = true)
+	private String password;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", userName=" + userName + ", password=" + password + "]";
+	}
+
+
+}
