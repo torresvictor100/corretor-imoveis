a22ca542530317808ece31ec678b74c453bb6061
João Victor Torres <torresvictor100@gmail.com>
2022-04-25 14:36:04 -0300

    Corrigido o problema para enviar a foto para o s3

--- src/main/java/com/project/corretor/imoveis/corretor/imoveis/CorretorImoveisApplication.java
+++ src/main/java/com/project/corretor/imoveis/corretor/imoveis/CorretorImoveisApplication.java
@@ -1,13 +1,25 @@
 package com.project.corretor.imoveis.corretor.imoveis;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.project.corretor.imoveis.corretor.imoveis.service.S3Service;
+
 @SpringBootApplication
-public class CorretorImoveisApplication {
+public class CorretorImoveisApplication implements CommandLineRunner {
+	
+	@Autowired
+	private S3Service s3Service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CorretorImoveisApplication.class, args);
+	}
+	
+	@Override
+	public void run(String... args) throws Exception {		
+		s3Service.uploadFile("/home/joao/Imagens/foto.jpg");
 	}
 }
 	
--- src/main/java/com/project/corretor/imoveis/corretor/imoveis/config/SecurityConfig.java
+++ src/main/java/com/project/corretor/imoveis/corretor/imoveis/config/SecurityConfig.java
@@ -34,6 +34,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
 	private static final String[] PUBLIC_MATCHERS_POST = {
 			"/user",
+			"/photos",
+			"/photos/**"
 			
 	};
 	
